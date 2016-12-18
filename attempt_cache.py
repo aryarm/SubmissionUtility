df9700f897b59e0ec3b8008cd9857ac912c28110
meanmail <meanmail@mail.ru>
2016-12-19 06:14:23 +0700

    fix: if attempt_file not exists was exceptions
    Signed-off-by: meanmail <meanmail@mail.ru>

--- attempt_cache.py
+++ attempt_cache.py
@@ -5,34 +5,47 @@ _file_manager = FileManager()
 
 
 def clear():
-    _file_manager.write_json(ATTEMPT_FILE, {})
-
-
-def get_step_id():
-    file = _file_manager.read_json(ATTEMPT_FILE)
-    position = file['current_position']
-    return file['steps'][position - 1]
-
-
-def set_lesson_id(lesson_id):
-    data = _file_manager.read_json(ATTEMPT_FILE)
-    data['lesson_id'] = lesson_id
-    _file_manager.write_json(ATTEMPT_FILE, data)
-
-
-def get_lesson_id():
-    file = _file_manager.read_json(ATTEMPT_FILE)
-    return file['lesson_id']
-
-
-def get_current_position():
-    file = _file_manager.read_json(ATTEMPT_FILE)
-    return file['current_position']
+    set_data({})
 
 
 def get_data():
-    return _file_manager.read_json(ATTEMPT_FILE)
+    try:
+        return _file_manager.read_json(ATTEMPT_FILE)
+    except FileNotFoundError:
+        clear()
+        return {}
 
 
 def set_data(data):
     _file_manager.write_json(ATTEMPT_FILE, data)
+
+
+def get_step_id():
+    data = get_data()
+    try:
+        position = data['current_position']
+        return data['steps'][position - 1]
+    except KeyError:
+        return None
+
+
+def set_lesson_id(lesson_id):
+    data = get_data()
+    data['lesson_id'] = lesson_id
+    set_data(data)
+
+
+def get_lesson_id():
+    data = get_data()
+    try:
+        return data['lesson_id']
+    except KeyError:
+        return None
+
+
+def get_current_position():
+    data = get_data()
+    try:
+        return data['current_position']
+    except KeyError:
+        return None
