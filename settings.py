cc8b7d27476fae268842d86f4ba968c7e6dded6b
meanmail <meanmail@mail.ru>
2016-12-18 07:26:43 +0700

    Created settings
    Signed-off-by: meanmail <meanmail@mail.ru>

---
+++ settings.py
@@ -0,0 +1,10 @@
+STEPIK_HOST = "https://stepik.org/"
+STEPIK_API_URL = STEPIK_HOST + "api"
+APP_FOLDER = ".stepik"
+CLIENT_FILE = APP_FOLDER + "/client_file"
+ATTEMPT_FILE = APP_FOLDER + "/attempt_file"
+CLIENT_ID = "ixM0Tj0S2O73QLyWzMzvxOyRmxYvV3lMbw3VvDHX"
+CLIENT_SECRET = "tD8vwpY7PHu3wbccy4wYnWr1edeLkvjnxRH0ajSeK2Nsz5bPs23ScHwbhets6Q" \
+                "O7xUpj40T4PLVy5w5rChang4YfEwtgQLkkPJ6nO1NUJTob0K9xXjPlzWrhhonT8kbj"
+GRAND_TYPE_PASSWORD = "password"
+GRAND_TYPE_CREDENTIALS = "client_credentials"
