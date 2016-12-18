dfa97ec4df4b17baccb3b145faa9ad30bce29347
meanmail <meanmail@mail.ru>
2016-12-19 01:40:06 +0700

    Added new utils
    Signed-off-by: meanmail <meanmail@mail.ru>

--- utils.py
+++ utils.py
@@ -32,3 +32,40 @@ def get_step_id(step_url):
     if match is None:
         return 0
     return int(match.group(1))
+
+
+def prepare_ids(ids):
+    return "&".join(map(lambda id: "ids[]=" + str(id), ids))
+
+
+def entities_loader(getter, user, key, ids, entity_class):
+    has_next = True
+    page_index = 1
+    entities = list()
+
+    while has_next:
+        page = getter(user, ids, page_index)
+        has_next = page['meta']['has_next']
+        page_index += 1
+        page = page[key]
+        entities_list = list(map(lambda entity: entity_class(user, entity), page))
+        entities.extend(entities_list)
+
+    return entities
+
+
+def all_entities_loader(getter, user, key, entity_class, **kwargs):
+    has_next = True
+    page_index = 1
+    entities = list()
+
+    while has_next:
+        kwargs['page'] = page_index
+        page = getter(user, **kwargs)
+        has_next = page['meta']['has_next']
+        page_index += 1
+        page = page[key]
+        entities_list = list(map(lambda entity: entity_class(user, entity), page))
+        entities.extend(entities_list)
+
+    return entities
