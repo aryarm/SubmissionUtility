f1159202989327731287256bd2b8457fd8b25e0f
meanmail <meanmail@mail.ru>
2016-12-18 07:29:17 +0700

    Changed setup.py: added package, inc version
    Signed-off-by: meanmail <meanmail@mail.ru>

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(
     name='submitter',
-    version='0.2',
+    version='0.3',
     py_modules=['submitter'],
     install_requires=[
         'Click',
         'requests',
         'colorama',
+        'html2text'
     ],
     entry_points='''
         [console_scripts]
