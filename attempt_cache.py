45071601654958479a00cdbf01ea517d3be732d0
meanmail <meanmail@mail.ru>
2016-12-18 07:26:21 +0700

    Extract Attempt file utils
    Signed-off-by: meanmail <meanmail@mail.ru>

---
+++ attempt_cache.py
@@ -0,0 +1,49 @@
+from filemanager import FileManager
+from settings import ATTEMPT_FILE
+
+_file_manager = FileManager()
+
+
+def set_attempt_id(attempt_id):
+    data = _file_manager.read_json(ATTEMPT_FILE)
+    data['attempt_id'] = attempt_id
+    _file_manager.write_json(ATTEMPT_FILE, data)
+
+
+def get_attempt_id():
+    file = _file_manager.read_json(ATTEMPT_FILE)
+    return file['attempt_id']
+
+
+def clear():
+    _file_manager.write_json(ATTEMPT_FILE, {})
+
+
+def get_step_id():
+    file = _file_manager.read_json(ATTEMPT_FILE)
+    position = file['current_position']
+    return file['steps'][position - 1]
+
+
+def set_lesson_id(lesson_id):
+    data = _file_manager.read_json(ATTEMPT_FILE)
+    data['lesson_id'] = lesson_id
+    _file_manager.write_json(ATTEMPT_FILE, data)
+
+
+def get_lesson_id():
+    file = _file_manager.read_json(ATTEMPT_FILE)
+    return file['lesson_id']
+
+
+def get_current_position():
+    file = _file_manager.read_json(ATTEMPT_FILE)
+    return file['current_position']
+
+
+def get_data():
+    return _file_manager.read_json(ATTEMPT_FILE)
+
+
+def set_data(data):
+    _file_manager.write_json(ATTEMPT_FILE, data)
