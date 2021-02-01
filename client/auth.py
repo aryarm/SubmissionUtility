5999576db01af9e3c460c476e799f5b8c3a3cce2
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2021-01-31 22:29:10 -0800

    revert authentication to passwordless setup

--- client/auth.py
+++ client/auth.py
@@ -1,21 +1,32 @@
 import requests
+import datetime
 
 from utils import exit_util
 
 
-def auth_user_password(user, password):
+def auth_user_password(user, password=None):
     try:
-        data = {'grant_type': user.grand_type,
-                'client_id': user.client_id,
-                'secret_id': user.secret,
-                'username': user.username,
-                'password': password}
-        resp = requests.post('https://stepik.org/oauth2/token/', data)
+        expiration = datetime.datetime.now()
+        if password:
+            data = {'grant_type': user.grand_type,
+                    'client_id': user.client_id,
+                    'secret_id': user.secret,
+                    'username': user.username,
+                    'password': password}
+            resp = requests.post('https://stepik.org/oauth2/token/', data)
+        else:
+            auth = requests.auth.HTTPBasicAuth(user.client_id, user.secret)
+            data = {'grant_type': user.grand_type}
+            resp = requests.post('https://stepik.org/oauth2/token/', data, auth=auth)
 
         assert resp.status_code < 300
 
-        user.access_token = (resp.json())['access_token']
-        user.refresh_token = (resp.json())['refresh_token']
+        resp = resp.json()
+        user.access_token = resp['access_token']
+        user.expiration = expiration + datetime.timedelta(seconds=int(resp['expires_in']))
+        user.refresh_token = None
+        if 'refresh_token' in resp:
+            user.refresh_token = resp['refresh_token']
         user.save()
     except AssertionError:
         exit_util("Check your authentication.")
--- models/user.py
+++ models/user.py
@@ -1,4 +1,6 @@
-from client.consts import GRAND_TYPE_PASSWORD
+import datetime
+from client.auth import auth_user_password
+from client.consts import GRAND_TYPE_CREDENTIALS
 from filemanager import FileManager
 from settings import CLIENT_FILE, CLIENT_ID, CLIENT_SECRET
 
@@ -15,10 +17,16 @@ class User:
         self.client_id = data.get('client_id', CLIENT_ID)
         self.secret = data.get('client_secret', CLIENT_SECRET)
         self.username = data.get('username', 'Unknown')
-        self.grand_type = data.get('grand_type', GRAND_TYPE_PASSWORD)
+        self.grand_type = data.get('grand_type', GRAND_TYPE_CREDENTIALS)
         self.access_token = data.get('access_token', '')
         self.refresh_token = data.get('refresh_token', '')
-        self.step_type = data.get('step_type', 'code')
+        self.expiration = data.get('expiration', datetime.datetime.min)
+        if not isinstance(self.expiration, datetime.datetime):
+            self.expiration = datetime.datetime.fromisoformat(self.expiration)
+        self.step_type = data.get('step_type', 'all')
+
+        if self.expiration < datetime.datetime.now() and self.client_id and self.secret:
+            auth_user_password(self)
 
         self.save()
 
@@ -30,6 +38,7 @@ class User:
         data['grand_type'] = self.grand_type
         data['access_token'] = self.access_token
         data['refresh_token'] = self.refresh_token
+        data['expiration'] = self.expiration.isoformat()
         data['step_type'] = self.step_type
 
         self.file_manager.write_json(CLIENT_FILE, data)
