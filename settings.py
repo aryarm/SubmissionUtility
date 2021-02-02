c92892bb3a6e43ceafb884e0c37b6badfc5ce0ce
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2021-02-02 13:08:18 -0800

    delete settings.py from old repo

--- settings.py
+++
@@ -1,6 +0,0 @@
-APP_FOLDER = ".stepik"
-CLIENT_FILE = APP_FOLDER + "/client_file"
-ATTEMPT_FILE = APP_FOLDER + "/attempt_file"
-CLIENT_ID = "ixM0Tj0S2O73QLyWzMzvxOyRmxYvV3lMbw3VvDHX"
-CLIENT_SECRET = "tD8vwpY7PHu3wbccy4wYnWr1edeLkvjnxRH0ajSeK2Nsz5bPs23ScHwbhets6Q" \
-                "O7xUpj40T4PLVy5w5rChang4YfEwtgQLkkPJ6nO1NUJTob0K9xXjPlzWrhhonT8kbj"
