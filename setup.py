1f867c13eaa0237098bcc901a378814626ef58e0
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2021-02-01 22:41:11 -0800

    try to create meta.yaml package for conda package

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='stepik',
-    version='0.1',
+    version='0.1.0',
     py_modules=['stepik'],
     install_requires=[
-        'Click',
+        'click',
         'requests',
         'colorama',
         'html2text'
