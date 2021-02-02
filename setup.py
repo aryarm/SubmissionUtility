ed0e81b192405cb1f25ec960a300377edbc6053b
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2021-02-02 10:45:35 -0800

    upload to conda from pypi using conda skeleton

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='stepik-cli',
-    version='0.3.0',
+    version='0.3.1',
     description="A Stepik CLI for students",
     long_description=long_description,
     long_description_content_type="text/markdown",
